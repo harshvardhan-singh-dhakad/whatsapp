661ffc2a435e8d462c25b06d7e929c1b7200e7ea
Harshvardhan Dhakad <harshvardhan@adsverse.in>
2026-05-14 14:05:08 +0530

    Update PM2 config name to adsverse-bot

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'adsverse-shivani',
+      name: 'adsverse-bot',
       script: './index.js',
       instances: 1,
       autorestart: true,
@@ -11,8 +11,8 @@ module.exports = {
         NODE_ENV: 'production',
       },
       // Logs management
-      error_file: './leads/pm2-error.log',
-      out_file: './leads/pm2-out.log',
+      error_file: './leads/pm2-error-0.log',
+      out_file: './leads/pm2-out-0.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
     },
   ],
